e3f58368614b4f08bc6f1fc12169b6d794e262bf
Tommo Zhou <tommo.zhou@gmail.com>
2015-05-01 11:33:21 +0800

    fix curve crash due to incorrect time clamping

--- src/moai-sim/MOAIAnimCurveBase.cpp
+++ src/moai-sim/MOAIAnimCurveBase.cpp
@@ -257,6 +257,7 @@ float MOAIAnimCurveBase::WrapTime ( float t, float &repeat ) const {
 
 	float time = ( t - startTime ) / length; // normalize time
 	float wrappedT = 0.0f;
+
 	repeat = 0.0f;
 
 	switch ( mWrapMode ) {
@@ -287,12 +288,13 @@ float MOAIAnimCurveBase::WrapTime ( float t, float &repeat ) const {
 		}
 		break;
 	}
-
-	wrappedT = wrappedT * length + startTime;
+	float result = wrappedT * length + startTime;
 	
-	if ( wrappedT + EPSILON > t && wrappedT - EPSILON < t ) { 
-		wrappedT = t; 
+	if( wrappedT != 0.0f && wrappedT != 1.0f ) {
+		if ( result + EPSILON > t && result - EPSILON < t ) { 
+			result = t; 
+		}
 	}
 
-	return wrappedT;
+	return result;
 }
